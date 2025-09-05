1d5d940c2e06404778b90ff383c68961411ed3f8
manoel barros <manoelneto400@gmail.com>
2025-09-05 13:26:22 -0300

    Fix: Organiza categoria dos veiculos

--- src/main/java/org/example/model/entity/CategoriaVeiculo.java
+++ src/main/java/org/example/model/entity/CategoriaVeiculo.java
@@ -2,10 +2,10 @@ package org.example.model.entity;
 
 public enum CategoriaVeiculo {
     UBER_X("UberX", "Corrida mais econômica"),
-    COMFORT("Uber Comfort", "Carros mais novos e espaçosos"),
-    BLACK("Uber Black", "Veículos premium"),
-    BAG("Uber Bag", "Veículos com porta-malas maior"),
-    XL("Uber XL", "Capacidade para mais passageiros");
+    UBER_COMFORT("Uber Comfort", "Carros mais novos e espaçosos"),
+    UBER_BLACK("Uber Black", "Veículos premium"),
+    UBER_BAG("Uber Bag", "Veículos com porta-malas maior"),
+    UBER_XL("Uber XL", "Capacidade para mais passageiros");
 
     private final String nome;
     private final String descricao;
