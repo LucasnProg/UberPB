977ecf1022b02769ba5d2cb117fd519ffaad17d5
LucasnProg <lucasnprog@gmail.com>
2025-10-22 18:44:08 -0300

    fix: correção no CLI de avaliações

--- src/main/java/org/example/view/Mapa/dados_viagem.js
+++ src/main/java/org/example/view/Mapa/dados_viagem.js
@@ -1,3 +1,3 @@
 var origem = { lat: -7.204100, lon: -35.911500, desc: "Universidade Estadual da Paraíba (UEPB) - Campus I" };
-var destino = { lat: -7.210700, lon: -35.901800, desc: "Partage Shopping" };
-var carro_pos = { lat: -7.210700, lon: -35.901800 };
+var destino = { lat: -7.219200, lon: -35.908300, desc: "Universidade Federal de Campina Grande (UFCG)" };
+var carro_pos = { lat: -7.219200, lon: -35.908300 };
